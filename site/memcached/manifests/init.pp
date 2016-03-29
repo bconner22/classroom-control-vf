de15fe2d31322ccf5dd8e5f6135b02a61bf2a11a
Brian <bconner22@gmail.com>
2016-03-29 13:08:46 -0500

    Create init.pp

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,14 @@
+class memcached {
+  package {'memcached': 
+    ensure => installed,
+  }
+  file {'/etc/sysconfig/memcached':
+    ensure => present,
+    source => 'puppet:///modules/memcached/memcached',
+    notify => Service['memcached'],
+  }
+  service {'memcached'
+    ensure => running, 
+    require => Package['memcached'],
+
+}
