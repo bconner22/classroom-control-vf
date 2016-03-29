f9206c4a60c13c099589ccbdc448cd0ce25927c3
Brian <bconner22@gmail.com>
2016-03-29 14:12:25 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -3,8 +3,27 @@ class nginx {
     ensure => installed,
   }
   case $os.family { 
-    'Redhat' 
-
-
+    'RedHat': {
+      file {'/var/www':
+        ensure => directory,
+      }
+      file {'/var/www/index.html': 
+        ensure => present,
+        source => 'puppet:///modules/nginx/index.html',
+        notify => Service['nginx'],
+      }
+      file {'/etc/nginx/nginx.conf':
+        ensure => present,
+        source => 'puppet:///modules/nginx/nginx.conf',
+        notify => Service['nginx'],
+      }
+      file {'/etc/nginx/conf.d/default.conf':
+        ensure => present,
+        source => 'puppet:///modules/nginx/default.conf',
+        notify => Service['nginx'],
+      }
+    }
+    'Debian': { notify {"not yet supported"} }
+  }
 
 }
