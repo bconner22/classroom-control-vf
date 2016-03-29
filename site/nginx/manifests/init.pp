5b83346428a32f750aac0303686c1f7ccc443fdc
Brian <bconner22@gmail.com>
2016-03-29 14:37:36 -0500

    Refactored notifies into one subscribe

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,24 +10,25 @@ class nginx {
       file {'/var/www/index.html': 
         ensure => present,
         source => 'puppet:///modules/nginx/index.html',
-        notify => Service['nginx'],
+        
       }
       file {'/etc/nginx/nginx.conf':
         ensure => present,
         source => 'puppet:///modules/nginx/nginx.conf',
-        notify => Service['nginx'],
+        
       }
       file {'/etc/nginx/conf.d/default.conf':
         ensure => present,
         source => 'puppet:///modules/nginx/default.conf',
-        notify => Service['nginx'],
+       
       }
     }
     'Debian': { notify {"not yet supported":} }
   }
   service {'nginx':
-    ensure => running,
-    enable => true,
+    ensure    => running,
+    enable    => true,
+    subscribe => [File['/etc/nginx/conf.d/default.conf'], File['/etc/nginx/nginx.conf'], File['/var/www/index.html']],
   }  
 
 }
