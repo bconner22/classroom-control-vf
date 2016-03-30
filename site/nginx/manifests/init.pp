0accbfc419cb9a5c8707b4f568a2b127129bfd4e
Brian <bconner22@gmail.com>
2016-03-30 15:26:16 -0500

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -10,7 +10,7 @@ class nginx {
       $nginxuser = 'www-data'
       )
     }
-    ['RedHat', 'Debian']: { 
+    'RedHat', 'Debian': { 
       ( 
       $nginxpackage = 'nginx'
       $nginxfileown = 'root'
